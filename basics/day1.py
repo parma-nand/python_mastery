214989f630aa7ec6e416188748a18ea3e557e36f
parma-nand <real.me.parma@gmail.com>
2026-04-18 02:51:38 +0530

    List and Tuple

--- basics/day1.py
+++ basics/day1.py
@@ -1,1 +1,43 @@
 print("Shri Ganeshay Namah")
+
+a,b,c="hey"
+print(a)
+print(b)
+
+d=1,2,3
+print(d)
+p,q,r=d
+print(p)
+print(q)
+print(r)
+
+# Swapping
+a=2
+b=3
+a,b=b,a
+print(a,b)
+
+# Function Packing,Unpacking
+def get_data():
+    return 100,200 #Packing
+x,y=get_data()
+print(x)
+print(y)
+
+# Mutability
+a=[1,2,3]
+print(a)
+a.append(4)
+print(a)
+a.remove(2)
+print(a)
+a[0]=5
+print(a)
+
+b=(1,2,3)   
+print(b)
+
+t=([1],3)
+print(t)
+t[0].append(6)
+print(t)
